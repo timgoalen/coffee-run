5e8ad0dd58b4338fac7e012cf59eccc7eba14ada
Tim Goalen <timgoalen@hotmail.com>
2023-07-10 11:56:34 +0000

    Add all docstrings and line comments

--- run.py
+++ run.py
@@ -11,9 +11,12 @@ from tabulate import tabulate
 from termcolor import colored
 from pyfiglet import Figlet
 
+
 # GLOBAL VARIABLES
 
-# from Code Institute's 'love sandwiches' tutorial:
+
+# Import Google Sheets API,
+# code from Code Institute's 'love sandwiches' tutorial.
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -29,23 +32,28 @@ COFFEE_MENU = SHEET.worksheet('coffee_menu')
 COFFEE_DATA = COFFEE_MENU.get_all_values()
 ORDERS_SPREADSHEET = SHEET.worksheet('orders')
 
+# Store user data before sending to the Google Sheet database.
 order_list = []
 cost_list = []
-# for the title effect to not happen after 1st time...[rephrase]
+
+# A 'switch' to choose whether or not the title text is displayed
+# with staggered timings.
 title_print_delay = True
+
 
 # UTILITY FUNCTIONS
 
 
 def clear_screen():
     """
-    Clears the terminal
+    Clear the terminal.
     """
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def typing_effect(text):
     """
+    Create a staggered typing effect.
     """
     for character in text:
         sys.stdout.write(character)
@@ -55,6 +63,7 @@ def typing_effect(text):
 
 def get_user_integer_input(prompt):
     """
+    Validate user input by checking it's an integer.
     """
     while True:
         try:
@@ -62,7 +71,6 @@ def get_user_integer_input(prompt):
             return user_input
         except ValueError:
             print("Sorry, that's not a digit")
-            # need 'continue' here?
 
 
 # PROCEDURAL FUNCTIONS
@@ -70,6 +78,7 @@ def get_user_integer_input(prompt):
 
 def title():
     """
+    Display the program title.
     """
     f = Figlet(font="small")
     title1 = "COMMAND"
@@ -85,8 +94,9 @@ def title():
     print(colored(f.renderText(title3), "light_green"))
 
 
-def welcome():
+def introduction():
     """
+    Display the introductory message.
     """
     time.sleep(1.5)
     typing_effect("Why wait in line?!")
@@ -97,17 +107,23 @@ def welcome():
 
 def enter():
     """
+    Ask user if they want to see the menu.
     """
+
+    # Clear screen and display the title again,
+    # use `title_print_delay` to turn the staggered printing off.
     global title_print_delay
     title_print_delay = False
     clear_screen()
     title()
 
+    # Display input dialogue.
     print("Shall I show you the menu?\n")
     print(colored("[Y] - Hell yes!", "light_green"))
     print(colored("[N] - Nah, don't know how I got here\n", "light_red"))
     time.sleep(1)
 
+    # Validate user input ("y" or "n").
     while True:
         customer_input = input("...press Y or N, then Enter: ").strip().lower()
         if customer_input == "y":
@@ -127,44 +143,55 @@ def enter():
 
 def display_coffee_menu():
     """
-    Displays coffee menu from google sheets
+    Display the coffee menu from Google Sheets.
     """
     clear_screen()
-    print(tabulate(COFFEE_DATA, headers="firstrow", tablefmt="grid", numalign="right"))
+    # Use Tabulate to format the data into a table.
+    print(tabulate(COFFEE_DATA,
+                   headers="firstrow", tablefmt="grid", numalign="right"))
     print()
+
     time.sleep(1)
     choose_coffee()
 
 
 def choose_coffee():
     """
+    Choose coffee type and add to `order_list` global variable.
     """
     while True:
+        # Display input dialogue.
         user_input = get_user_integer_input("Choose an option # (1-6): \n")
 
+        # Validate input between 1-6.
         if 1 <= user_input <= 6:
-            # user input "+ 1" to take account of the spreadsheet title row
+            # "+1" to take account of the spreadsheet title row.
             row_index = user_input + 1
+            # Get coffee name from spreadsheet & update `order_list` variable.
             choice = COFFEE_MENU.cell(row_index, 1).value
-            cost = COFFEE_MENU.cell(row_index, 2).value
             order_list.append(choice)
+            # Get coffee price from spreadsheet & update `cost_list` variable.
+            cost = COFFEE_MENU.cell(row_index, 2).value
             cost_list.append(cost)
             print(f"Thanks, you chose {colored(choice, 'light_green')}")
             break
-        else:
-            print("Whoops, the number must be between 1-6")
+        print("Whoops, the number must be between 1-6")
 
     choose_quantity()
 
 
 def choose_quantity():
     """
+    Choose coffee quantity.
     """
     while True:
+        # Display input dialogue.
         quantity = get_user_integer_input("How many of these would you like? (1-10): \n")
-        
+        # Validate input between 1-10.
         if 1 <= quantity <= 10:
+            # Display feedback on user action.
             print(colored(f"You'd like {quantity} of these", "light_green"))
+            # Update `order_list` and `coffee_list` variables.
             order_list.append(quantity)
             cost_list.append(quantity)
             break
@@ -176,9 +203,12 @@ def choose_quantity():
 
 def add_more_coffees():
     """
+    Give option to add an additional coffee type.
     """
     while True:
+        # Display input dialogue.
         customer_input = input("Would you like to add more to the order? [y/n]:\n").strip().lower()
+        # Validate input as "y" or "n".
         if customer_input == "y":
             choose_coffee()
             break
@@ -192,16 +222,19 @@ def add_more_coffees():
 
 def add_customer_name():
     """
+    Ask for customer name.
     """
     clear_screen()
 
     while True:
+        # Display input dialogue.
         customer_name = input("What name should we write on your order?: \n")
-
+        # Validate input as a string between 1-30 characters long.
         if len(customer_name) < 1 or len(customer_name) > 30:
             print("Please enter a name between 1-30 characters long")
             continue
         else:
+            # Add name to start of `order_list`.
             capitalized_name = customer_name.capitalize()
             order_list.insert(0, capitalized_name)
             break
@@ -211,26 +244,37 @@ def add_customer_name():
 
 def send_order(order):
     """
+    Update Google Sheet with all user data.
     """
     ORDERS_SPREADSHEET.append_row(order)
+
     display_pending_order()
 
 
 def display_pending_order():
     """
+    Get data on last order from Google Sheet and display in a table.
     """
     clear_screen()
-
+    # Find the last row in the Google Sheet.
     pending_orders = ORDERS_SPREADSHEET.get_all_values()
     last_order_row = len(pending_orders)
-    last_order_items = ORDERS_SPREADSHEET.row_values(last_order_row)    
+    # Retrieve the data.
+    last_order_items = ORDERS_SPREADSHEET.row_values(last_order_row)
+    # Print the customer name, stored as the first cell in the row.
     print(f"Thanks {last_order_items[0]}, your order is:\n")
 
+    # Format the retrieved row, splitting every other value into
+    # `coffees` or `quantities` lists.
     coffees = last_order_items[1:-1:2]
     quantities = last_order_items[2::2]
+    # Create a new list of tuples, by pairing each item in `coffees` with its
+    # corresponding quantity.
     receipt = list(zip(coffees, quantities))
+    # Use Tabulate to print table.
     print(tabulate(receipt, headers=["Coffee", "Quantity"], tablefmt="grid"))
 
+    # Display cost, pickup time and order time.
     calculate_total_cost()
     calculate_pickup_time(quantities)
     display_current_time()
@@ -241,6 +285,7 @@ def display_pending_order():
 
 def press_enter_to_exit():
     """
+    Provide option to exit the program.
     """
     while True:
         user_input = input("\n\nPress Enter to EXIT:\n")
@@ -254,11 +299,12 @@ def press_enter_to_exit():
 
 def calculate_pickup_time(quantities):
     """
+    Calculate the number of minutes the customer should wait before collection,
+    use 1 minute per 1 coffee.
     """
-    # convert 'quantities' list of strings to list of integers, using map()
-    # https://www.geeksforgeeks.org/python-converting-all-strings-in-list-to-integers/
+    # Convert `quantities` list of strings into a list of integers.
     quantities_int = list(map(int, quantities))
-
+    # Calculate total quantity of coffee ordered.
     x = 0
     for num in quantities_int:
         x = x + num
@@ -270,15 +316,20 @@ def calculate_pickup_time(quantities):
 
 def calculate_total_cost():
     """
+    Calculate the total cost of the order.
     """
+    # Get the prices.
     unit_price_unformatted = cost_list[::2]
-    # Remove the '£' from the price, and convert to float
+    # Remove the "£" from the price, and convert into floats.
     unit_price_list = [float(price[1:]) for price in unit_price_unformatted]
 
+    # Get the quantities.
     quantities = cost_list[1::2]
-    # Convert the values in the list into integers
+    # Convert the values in the list into integers.
     quantities = [int(quantity) for quantity in quantities]
 
+    # Calculate the total by combining each price
+    # with its corresponding quantity.
     total = 0
     for price, quantity in zip(unit_price_list, quantities):
         subtotal = price * quantity
@@ -290,6 +341,7 @@ def calculate_total_cost():
 
 def display_current_time():
     """
+    Display the time when the order was made.
     """
     london_timezone = pytz.timezone('Europe/London')
     london_datetime = datetime.now(london_timezone)
@@ -299,7 +351,9 @@ def display_current_time():
 
 def goodbye_message():
     """
+    Display the title and thank you message.
     """
+    # Use `title_print_delay` variable to turn the staggered printing off.
     global title_print_delay
     title_print_delay = True
     clear_screen()
@@ -310,11 +364,13 @@ def goodbye_message():
 
 def main():
     """
+    Call the first 4 functions.
     """
     clear_screen()
     title()
-    welcome()
+    introduction()
     enter()
 
 
+# Start the program.
 main()
